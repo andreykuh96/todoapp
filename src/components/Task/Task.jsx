76c2db63d210720ece8ae9d81adfd2d312ce13f4
Andrey <andrey@MacBook-Air-Andrey.local>
2023-11-28 17:13:28 +0300

    timer with FC

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,74 +1,60 @@
 import { formatDistanceToNow } from 'date-fns';
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 
-export default class Task extends Component {
-  static defaultProps = {
-    onChangeActiveTask: () => {
-      return alert('To add a new task, pass the function');
-    },
-    onDeleteTask: () => {
-      return alert('To add a new task, pass the function');
-    },
+const Task = ({
+  taskProps,
+  tick,
+  editingTask,
+  id,
+  onChangeActiveTask,
+  onDeleteTask,
+  onChangeEditing,
+  onStartTimer,
+  onStopTimer,
+}) => {
+  const [value, setValue] = React.useState(taskProps.description);
+
+  React.useEffect(() => {
+    let timerID = setInterval(() => tick(), 1000);
+    return () => {
+      clearInterval(timerID);
+    };
+  }, []);
+
+  const onInputChange = (e) => {
+    setValue(e.target.value);
   };
 
-  static propTypes = {
-    taskProps: PropTypes.object,
-    onChangeActiveTask: PropTypes.func,
-    onDeleteTask: PropTypes.func,
-    editingTask: PropTypes.func,
-    id: PropTypes.number,
-    onChangeEditing: PropTypes.func,
-  };
-
-  state = {
-    value: this.props.taskProps.description,
-  };
-
-  componentDidMount() {
-    this.timerID = setInterval(() => this.props.tick(), 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  onInputChange = (e) => {
-    this.setState({
-      value: e.target.value,
-    });
-  };
-
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    this.props.editingTask(this.props.id, this.state.value);
+    editingTask(id, value);
   };
 
-  render() {
-    const { taskProps, onChangeActiveTask, onDeleteTask, onChangeEditing } = this.props;
-    const { description, created, time } = taskProps;
-    const minutes = Math.floor(time / 1000 / 60);
-    const seconds = (time / 1000) % 60;
-    return (
-      <>
-        <div className="view">
-          <input onClick={onChangeActiveTask} className="toggle" type="checkbox" />
-          <label>
-            <span className="title">{description}</span>
-            <span className="description">
-              <button onClick={this.props.onStartTimer} className="icon icon-play"></button>
-              <button onClick={this.props.onStopTimer} className="icon icon-pause"></button>
-              {minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}
-            </span>
-            <span className="description">{formatDistanceToNow(created, { includeSeconds: true })}</span>
-          </label>
-          <button onClick={onChangeEditing} className="icon icon-edit"></button>
-          <button onClick={onDeleteTask} className="icon icon-destroy"></button>
-        </div>
-        <form onSubmit={this.onSubmit}>
-          <input type="text" className="edit" onChange={this.onInputChange} value={this.state.value} />
-        </form>
-      </>
-    );
-  }
-}
+  const { description, created, time } = taskProps;
+  const minutes = Math.floor(time / 1000 / 60);
+  const seconds = (time / 1000) % 60;
+
+  return (
+    <>
+      <div className="view">
+        <input onClick={onChangeActiveTask} className="toggle" type="checkbox" />
+        <label>
+          <span className="title">{description}</span>
+          <span className="description">
+            <button onClick={onStartTimer} className="icon icon-play"></button>
+            <button onClick={onStopTimer} className="icon icon-pause"></button>
+            {minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+          </span>
+          <span className="description">{formatDistanceToNow(created, { includeSeconds: true })}</span>
+        </label>
+        <button onClick={onChangeEditing} className="icon icon-edit"></button>
+        <button onClick={onDeleteTask} className="icon icon-destroy"></button>
+      </div>
+      <form onSubmit={onSubmit}>
+        <input type="text" className="edit" onChange={onInputChange} value={value} />
+      </form>
+    </>
+  );
+};
+
+export default Task;
